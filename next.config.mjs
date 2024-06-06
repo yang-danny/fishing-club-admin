5ecbbbcb63e469de9d537e28b713c3db539b63d7
yang-danny <yang_danny@hotmail.com>
2024-06-07 00:21:24 +1000

    update CORS config

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
         return [
             {
                 // matching all API routes
-                source: "/api/:path*",
+                source: "/api/products/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
